2c691d1c6a22ab9be9cb9d8b455bb3f4a0f970bd
Frédéric Bour <fred@tarides.com>
2022-01-01 10:52:04 +0100

    Access profinfo bits in interpreter and marshaller

--- runtime/interp.c
+++ runtime/interp.c
@@ -209,6 +209,22 @@ sp is a local copy of the global variable Caml_state->extern_sp. */
 static intnat caml_bcodcount;
 #endif
 
+#ifdef WITH_PROFINFO
+
+static uintnat next_profinfo;
+static inline uintnat pop_profinfo(void)
+{
+  uintnat result = next_profinfo;
+  next_profinfo = 0;
+  return result;
+}
+
+#undef Alloc_small
+#define Alloc_small(result, wosize, tag) \
+  Alloc_small_with_profinfo(result, wosize, tag, pop_profinfo())
+
+#endif /* WITH_PROFINFO */
+
 /* The interpreter itself */
 
 value caml_interprete(code_t prog, asize_t prog_size)
@@ -1142,6 +1158,13 @@ value caml_interprete(code_t prog, asize_t prog_size)
       accu = Field (meths, li-1);
       Next;
     }
+
+    Instruct(PROFINFO):
+#ifdef WITH_PROFINFO
+      next_profinfo = *pc;
+#endif
+      pc++;
+      Next;
 
 /* Debugging and machine control */
 
