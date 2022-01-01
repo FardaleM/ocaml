4c21118ecd1a129cc6828c686163180bb9873334
Frédéric Bour <fred@tarides.com>
2022-01-01 13:04:00 +0100

    Support tag descriptors in bytecode toolchain

--- file_formats/cmo_format.mli
+++ file_formats/cmo_format.mli
@@ -39,7 +39,9 @@ type compilation_unit =
     cu_primitives: string list;         (* Primitives declared inside *)
     mutable cu_force_link: bool;        (* Must be linked even if unref'ed *)
     mutable cu_debug: int;              (* Position of debugging info, or 0 *)
-    cu_debugsize: int }                 (* Length of debugging info *)
+    cu_debugsize: int;                  (* Length of debugging info *)
+    cu_tagl : Taglib.t list;            (* Tag descriptors *)
+  }
 
 (* Format of a .cmo file:
      magic number (Config.cmo_magic_number)
