da9d1d6038b167fb3da088555d8c93efbfa5edc2
Fardale <fardale+git@crans.org>
2022-04-04 00:50:11 +0200

    Dump roots + address and length of each chunks

--- runtime/dump.c
+++ runtime/dump.c
@@ -2,7 +2,6 @@
 #define CAML_INTERNALS
 
 #include "caml/compact.h"
-#include "caml/dump.h"
 #include "caml/eventlog.h"
 #include "caml/fail.h"
 #include "caml/major_gc.h"
@@ -11,6 +10,12 @@
 #include "caml/misc.h"
 #include "caml/roots.h"
 #include "caml/signals.h"
+
+void gc_compaction();
+void dump_roots(value root, value *dummy);
+void dump_chunks();
+
+static FILE *fp; // File of the dump
 
 /*
  * Useful stuff:
@@ -21,35 +26,75 @@
  */
 
 /* TODO use the function to darken all the reachable value in the dump */
-/* Take a ocaml string as input and dump heap in it */
-CAMLprim value caml_dump_chunks(value value_filename) {
+/* Take a ocaml string as input and dump the memory in it */
+CAMLprim value caml_full_dump(value value_filename) {
   CAMLparam1(value_filename);
 
-  // Compact memory first
-  gc_compaction();
-
-  FILE *fp; // File of the dump
-  char *dump_chunk; // This will hold the pointer to a chunk of memory
-
+  // Opening the file for the dump
   const char *filename = String_val(value_filename);
   fp = fopen(filename, "w");
 
-  dump_chunk = caml_heap_start; // The pointer to the firts chunk is caml_heap_start
+  // Check for error for the file
   if (fp == NULL) {
     fprintf(stderr, "File can not be opened\n");
     return Val_unit;
   }
 
+  // Compact memory first
+  gc_compaction();
+
+  // Dump the root
+  caml_do_roots(dump_roots, 1);
+
+  // NULL word to mark the end of the roots
+  value null = 0;
+  fwrite(&null, Bsize_wsize(1), 1, fp);
+
+  dump_chunks();
+
+  fclose(fp);
+  return Val_unit;
+}
+
+// TODO dump address and length first
+void dump_chunks() {
+  char *dump_chunk; // This will hold the pointer to a chunk of memory
+
+  dump_chunk =
+      caml_heap_start; // The pointer to the firts chunk is caml_heap_start
+
   while (dump_chunk != NULL) {
     /* Dump from the start of the chunk using Chunk_block
-     * Chunk_size return the byte for the given chunk
+     * Chunk_size return the size in byte for the given chunk
+     * Dump address, size and content
      */
-    printf("Printing a chunk\n");
+    fwrite(&Chunk_block(dump_chunk), Bsize_wsize(1), 1, fp);
+    fwrite(&Chunk_size(dump_chunk), Bsize_wsize(1), 1, fp);
     fwrite(Chunk_block(dump_chunk), 1, Chunk_size(dump_chunk), fp);
     dump_chunk = Chunk_next(dump_chunk);
   }
+}
 
-  fclose(fp);
+void dump_roots(value root, value *dummy) {
+#ifdef NO_NAKED_POINTERS
+  if (Is_block(root) && !Is_young(root)) {
+#else
+  if (Is_block(root) && Is_in_heap(root)) {
+#endif
+    fwrite(&root, Bsize_wsize(1), 1, fp);
+  }
+}
+
+// Look at sweep_slice in major_gc.c to know how to do it
+CAMLprim value caml_show_tag(value v) {
+  CAMLassert(v == Val_unit);
+  char *chunk, *hp;
+  chunk = caml_heap_start;
+
+  while (chunk != NULL) {
+    hp = chunk;
+    chunk = Chunk_next(chunk);
+  }
   return Val_unit;
 }
 
